94d2e6ce987fbf066f33e071fc25d504f37ad702
ralixt <98614258+ralixt@users.noreply.github.com>
2024-01-15 16:20:24 +0100

    question 2.9

---
+++ prisma/migrations/20240115151955_user_id_in_order/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - Added the required column `userId` to the `Order` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE "Order" ADD COLUMN     "userId" UUID NOT NULL;
